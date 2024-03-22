62b7f63d8863ef09f3e989464519514aa2d6ccc2
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-03-22 11:57:20 -0300

    Update change.py
    commit 2/3

--- change.py
+++ change.py
@@ -1,3 +1,10 @@
 def change():
     expense = 23.75
     money = 100
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos: ")
+    print(int(money-expense))
+    print("Centavos: ")
+    print(int(100*((money-expense)-int(money-expense))))
